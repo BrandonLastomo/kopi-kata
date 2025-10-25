3c8495af4c068a6c735b3d9f1e13e60508bde1fb
Brandon <brandonlastomo1@gmail.com>
2025-10-25 20:52:23 +0700

    redesign home page UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,126 +21,138 @@
 <body>
 
     <!-- HEADER -->
-    <header class="header">
-        <div id="menu-btn" class="fas fa-bars"></div>
-
-        <a href="#" class="logo">Kopi & Kata</a>
-
-        <nav class="navbar">
-            <a href="#home">home</a>
-            <a href="#about">about</a>
-            <a href="#menu">menu</a>
-            <a href="#review">review</a>
-            <a href="{{ route('book.index') }}">book</a>
+    <header>
+        <nav>
+            <p class="logo">Kopi&Kata</p>
+            <div class="pageLinks">
+                <a href="{{ route('home') }}/#home">home</a>
+                <a href="{{ route('home') }}/#about">about</a>
+                <a href="{{ route('home') }}/#menu">menu</a>
+                <a href="{{ route('home') }}/#review">review</a>
+                <a href="{{ route('book.index') }}">book</a>
+            </div>
+            <div class="userInfo">
+                @if (auth()->check())
+                    <p>{{ auth()->check() ? "Hi, ". auth()->user()->name : "Welcome!" }}</p> | 
+                    <a href="{{ route('logout') }}" class="log">Logout</a>
+                @else
+                    <a href="{{ route('login') }}" class="log">Login</a>
+                @endif
+            </div>
         </nav>
-
-        <div class="user-info">
-            <a href="#" class="btn">{{ auth()->check() ? "Hi, ". auth()->user()->name : "Welcome!" }}</a>
-            @if (auth()->check())
-                <a href="{{ route('logout') }}" class="btn">Logout</a>
-            @else
-                <a href="{{ route('login') }}" class="btn">Login</a>
-            @endif
-        </div>
     </header>
 
     <!-- HOME -->
     <section class="home" id="home">
-        <div class="row">
-            <div class="content">
-                <p>We got you covered with</p>
-                <h1>Coffee</h1>
-                <p>It is best to start your day with a cup of coffee. Discover the</p>
-                <p>best flavours coffee you will ever have. We provide the best</p>
-                <p>for our customers.</p>
-            </div>
+        <div class="content">
+            <p>We got you covered with Coffee.</p>
+            <p>It is best to start your day with a cup of coffee.</p>
+            <p>Discover the best flavours coffee you will ever have.</p>
+            <p>We provide the best for our customers.</p>
         </div>
     </section>
 
     <!-- ABOUT -->
     <section class="about" id="about">
-        <h1 class="heading">about us <span>why choose us</span></h1>
+        <h1 class="heading">about us</h1>
         <div class="content">
-            <h1 class="title">Discover the best coffee!</h1>
-            <p>Bean Hub is a coffee shop that provides you with quality coffee that helps</p>
-            <p>boost your productivity and helps build your mood. Having a cup of</p>
-            <p>coffee is good, but having a cup of real coffee is greater. There is no</p>
-            <p>doubt that you will enjoy this coffee more than others you have ever</p>
-            <p>tasted.</p>
+            <div class="abt1">
+                <h1 class="title">Discover the best coffee!</h1>
+                <p><span class="logo">Kopi & Kita</span> is a coffee shop that provides you with quality coffee that helps</p>
+                <p>boost your productivity and helps build your mood. Having a cup of</p>
+                <p>coffee is good, but having a cup of real coffee is greater. There is no</p>
+                <p>doubt that you will enjoy this coffee more than others you have ever</p>
+                <p>tasted.</p>
+            </div>
+            <div class="abt2">
+                <h1 class="title">Discover the best coffee!</h1>
+                <p><span class="logo">Kopi & Kita</span> is a coffee shop that provides you with quality coffee that helps</p>
+                <p>boost your productivity and helps build your mood. Having a cup of</p>
+                <p>coffee is good, but having a cup of real coffee is greater. There is no</p>
+                <p>doubt that you will enjoy this coffee more than others you have ever</p>
+                <p>tasted.</p>
+            </div>
+            <div class="abt3">
+                <h1 class="title">Discover the best coffee!</h1>
+                <p><span class="logo">Kopi & Kita</span> is a coffee shop that provides you with quality coffee that helps</p>
+                <p>boost your productivity and helps build your mood. Having a cup of</p>
+                <p>coffee is good, but having a cup of real coffee is greater. There is no</p>
+                <p>doubt that you will enjoy this coffee more than others you have ever</p>
+                <p>tasted.</p>
+            </div>
         </div>
     </section>
 
     <!-- MENU -->
     <section class="menu" id="menu">
-        <h1 class="heading">our menu <span>popular menu</span></h1>
+        <h1 class="heading">Reserve <span class="emp">Now</span>, Refreshed <span class="emp">Now.</span></h1>
 
-        <div class="box-container">
-            <a href="#" class="box">
-                <img src="src/images/menu-1.png" alt="">
-                <div class="content">
+        <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Espresso</h3>
                     <p>This drink is made by extracting compressed coffee grounds using high-pressure hot water.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
 
-            <a href="#" class="box">
-                <img src="src/images/menu-2.png" alt="">
-                <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Americano</h3>
                     <p>This drink is made by adding hot water to espresso.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
 
-            <a href="#" class="box">
-                <img src="src/images/menu-3.png" alt="">
-                <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Caffe Latte</h3>
                     <p>Made from espresso and steamed milk, caffe latte offers a smooth and creamy taste.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
 
-            <a href="#" class="box">
-                <img src="src/images/menu-4.png" alt="">
-                <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Cappuccino</h3>
                     <p>made from a combination of espresso, hot milk, and milk foam in balanced proportions.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
 
-            <a href="#" class="box">
-                <img src="src/images/menu-5.png" alt="">
-                <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Flat White</h3>
                     <p>made from espresso and steamed milk, with a higher ratio of coffee to milk than a latte.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
 
-            <a href="#" class="box">
-                <img src="src/images/menu-6.png" alt="">
-                <div class="content">
+            <div class="menuItem">
+                <div class="details">
                     <h3>Macchiato</h3>
                     <p>made from espresso with a little added milk foam on top.</p>
-                    <span>$8.99</span>
                 </div>
-            </a>
+                <p class="price">$8.99</p>
+                <img src="{{ asset('assets/images/menu2.png') }}" alt="menu">
+            </div>
         </div>
     </section>
 
     <!-- REVIEW -->
     <section class="review" id="review">
-        <h1 class="heading">reviews <span>what people says</span></h1>
+        <h1 class="heading">Don't Worry. We hear you.</h1>
 
         <div class="swiper review-slider">
             <div class="swiper-wrapper">
                 <div class="swiper-slide box">
-                    <i class="fas fa-quote-left"></i>
                     <i class="fas fa-quote-right"></i>
-                    <img src="src/images/pic-1.png" alt="">
+                    <p class="reviewText">tempat nya nyaman</p>
                     <div class="stars">
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
@@ -148,15 +160,12 @@
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
                     </div>
-                    <p>tempat nya nyaman</p>
-                    <h3>putra raka</h3>
-                    <span>satisfied client</span>
+                    <p>putra rakap - satisfied client</p>
                 </div>
 
                 <div class="swiper-slide box">
-                    <i class="fas fa-quote-left"></i>
                     <i class="fas fa-quote-right"></i>
-                    <img src="src/images/pic-2.png" alt="">
+                    <p class="reviewText">kopi nya enak coy</p>
                     <div class="stars">
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
@@ -164,15 +173,13 @@
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
                     </div>
-                    <p>kopi nya enak coy</p>
-                    <h3>kapka</h3>
-                    <span>satisfied client</span>
+                    <p>kapka - satisfied client</p>
                 </div>
 
                 <div class="swiper-slide box">
-                    <i class="fas fa-quote-left"></i>
+    
                     <i class="fas fa-quote-right"></i>
-                    <img src="src/images/pic-3.png" alt="">
+                    <p class="reviewText">tempatnya cozy</p>
                     <div class="stars">
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
@@ -180,9 +187,7 @@
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
                     </div>
-                    <p>tempatnya cozy</p>
-                    <h3>ilham</h3>
-                    <span>satisfied client</span>
+                    <p>ilham - satisfied client</p>
                 </div>
             </div>
             <div class="swiper-pagination"></div>
@@ -190,7 +195,7 @@
     </section>
 
     <!-- FOOTER -->
-    <section class="footer">
+    <footer>
         <div class="box-container">
             <div class="box">
                 <h3>quick links</h3>
@@ -219,8 +224,8 @@
             </div>
         </div>
 
-        <div class="credit">created by kelompok 7 | all rights reserved</div>
-    </section>
+        <p class="credit">created by kelompok 7 | all rights reserved</p>
+    </footer>
 
     <!-- SWIPER -->
     <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
